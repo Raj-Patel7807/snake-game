d85d229b9fe6c0225c029a0d1e6c7babaf66aead
Raj_Patel_7807 <rajpatel7807@gmail.com>
2026-04-21 19:05:19 +0530

    feat(food): upd food and add new foods

--- src/food/Food.cpp
+++ src/food/Food.cpp
@@ -1,11 +1,63 @@
 #include "Food.h"
 #include <cstdlib>
 
-void Food::spawn(int width, int height) {
-    position.x = rand() % width;
-    position.y = rand() % height;
+static bool contains(const std::vector<Point> &pts, const Point &p) {
+    for (const auto &x: pts) {
+        if (x == p) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void Food::spawn(int width, int height, const std::vector<Point> &forbidden, long long elapsedMs) {
+    int r = rand() % 100;
+
+    if (r < 70) {
+        type = FOOD_NORMAL;
+    } else if (r < 85) {
+        type = FOOD_SPECIAL;
+    } else if (r < 92) {
+        type = FOOD_POISON;
+    } else {
+        type = FOOD_BONUS_TIMED;
+    }
+
+    spawnTimeMs = elapsedMs;
+    ttlMs = 0;
+    if (type == FOOD_BONUS_TIMED) {
+        ttlMs = 7000; // 7 Seconds
+    }
+
+    Point p{};
+
+    for (int tries = 0; tries < 5000; tries++) {
+        p.x = 1 + rand() % (width - 2);
+        p.y = 1 + rand() % (height - 2);
+
+        if (!contains(forbidden, p)) {
+            position = p;
+            return;
+        }
+    }
+
+    position = p;
 }
 
 Point Food::getPosition() const {
     return position;
+}
+
+FoodType Food::getType() const {
+    return type;
+}
+
+bool Food::isExpired(long long elapsedMs) const {
+    if (type != FOOD_BONUS_TIMED) {
+        return false;
+    }
+    if (ttlMs <= 0) {
+        return false;
+    }
+    return (elapsedMs - spawnTimeMs) >= ttlMs;
 }
--- src/food/Food.h
+++ src/food/Food.h
@@ -1,11 +1,27 @@
 #pragma once
 #include "../utils/Point.h"
+#include <vector>
+
+enum FoodType {
+    FOOD_NORMAL = 1,
+    FOOD_SPECIAL = 2,
+    FOOD_POISON = 3,
+    FOOD_BONUS_TIMED = 4
+};
 
 class Food {
 private:
     Point position;
+    FoodType type = FOOD_NORMAL;
+    long long spawnTimeMs = 0;
+    int ttlMs = 0;
 
 public:
-    void spawn(int width, int height);
+    void spawn(int width, int height, const std::vector<Point>& forbidden, long long elapsedMs);
+
     Point getPosition() const;
+
+    FoodType getType() const;
+
+    bool isExpired(long long elapsedMs) const;
 };
