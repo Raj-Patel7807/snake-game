680317c2f73217f50a1143c11758e5d00ffbd1a1
Raj_Patel_7807 <rajpatel7807@gmail.com>
2026-04-21 19:04:52 +0530

    feat(config): add gameSettings

---
+++ src/config/GameSettings.h
@@ -0,0 +1,14 @@
+#pragma once
+
+enum GameMode {
+    MODE_CLASSIC = 1,
+    MODE_OBSTACLES = 2,
+    MODE_SURVIVAL = 3
+};
+
+struct GameSettings {
+    int delayMs;
+    bool wallsEnabled;
+    GameMode mode;
+};
+
