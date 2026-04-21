5ab8c3213fd917f5a2759eb6d1cdcbd7b1722745
Raj_Patel_7807 <rajpatel7807@gmail.com>
2026-04-21 19:08:10 +0530

    feat: upd main

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,37 @@
 #include "game/Game.h"
 #include "utils/animation/Animation.h"
+#include "ui/StartScreen.h"
+#include "ui/EndScreen.h"
+#include "persistence/HighScore.h"
+#include "utils/color/Color.h"
 #include <ctime>
 #include <iostream>
-#include <cstddef>
 
 int main() {
     srand(time(0));
+    Color::enableVirtualTerminal();
 
-    std::string name;
-    std::cout << "Enter your name: ";
-    getline(std::cin, name);
+    std::string name = StartScreen::welcomeAndGetName();
+    const std::string highScoreFile = "highscore.txt";
+    int maxScore = HighScore::load(highScoreFile);
+    bool playAgain = false;
+    GameSettings settings = StartScreen::askSettings();
 
-    animation(name);
+    do {
+        animation(name);
 
-    Game game;
-    game.run();
+        Game game;
+        game.run(name, settings.delayMs, settings.wallsEnabled, settings.mode, maxScore);
+
+        HighScore::save(highScoreFile, maxScore);
+        EndChoice end = EndScreen::showGameOverAndAskPlayAgain(name, maxScore);
+        playAgain = end.playAgain;
+        if (playAgain && end.changeSettings) {
+            settings = StartScreen::askSettings();
+        }
+    } while (playAgain);
+
+    EndScreen::sayGoodbye(name);
 
     return 0;
 }
